74801072b48f2dd9e1850bff2aea74411f95d1f1
Sarah Addo <sarahaddo@0587357137.wireless.umich.net>
2018-09-23 20:54:15 -0400

    restart website starting with mobile first - add resposive mobile hamburger nav

---
+++ script.js
@@ -0,0 +1,23 @@
+//Opens the navbar menu when hamburger icon is clicked
+function openMenu() {
+
+	document.getElementById("menu").classList.toggle("open");
+	document.getElementById("navbar-list").classList.toggle("show");
+	document.getElementById("bar1").classList.toggle("change");
+	document.getElementById("bar2").classList.toggle("change");
+	document.getElementById("bar3").classList.toggle("change");
+
+}
+
+//Closes the navbar menu when hamburger icon is clicked
+function closeMenu() {
+
+	document.getElementById("menu").classList.toggle("open");
+	document.getElementById("navbar-list").classList.toggle("show");
+	document.getElementById("bar1").classList.toggle("change");
+	document.getElementById("bar2").classList.toggle("change");
+	document.getElementById("bar3").classList.toggle("change");
+
+
+
+}
